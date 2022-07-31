1b2666f9685410f8fc4a8d85e16f83dc2c4f68a7
Oksana <prokoksa@gmail.com>
2022-07-31 14:19:18 +0300

    making changes to the mobile version

--- Дз7/js/burger-menu.js
+++ Дз7/js/burger-menu.js
@@ -1,7 +1,13 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
+let links = document.querySelector('.menu');
 
 menuBtn.addEventListener('click', function(){
     menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
+})
+
+links.addEventListener('click', function(){
+    menuBtn.classList.remove('active');
+    menu.classList.remove('active');
 })
