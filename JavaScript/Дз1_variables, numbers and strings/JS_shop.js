b6e8dc2a011a546a9ec4e04c1ad283dcfd6a90b1
Oksana <prokoksa@gmail.com>
2022-10-03 13:48:59 +0300

    corrections of remarks

--- JavaScript/Дз1_variables, numbers and strings/JS_shop.js
+++ JavaScript/Дз1_variables, numbers and strings/JS_shop.js
@@ -2,20 +2,20 @@
 let name = 'Телепорт бытовой VZHIH-101';
 let price = 10000;
 
-alert('В наличии имеется: "'+ name +'"');
-alert('Стоимость товара '+ price +' Q');
+alert(`В наличии имеется: " ${name} "`);
+alert(`Стоимость товара ${price} Q`);
 
 // Задача № 3
-let QuantityOfGoods = 2;
-let AmountOfDiscount = price * QuantityOfGoods * 0.1;
-let TotalPaymentAmount = QuantityOfGoods * price - AmountOfDiscount;
+let quantityGoods = 2;
+let amountDiscount = price * quantityGoods * 0.1;
+let totalPaymentAmount = quantityGoods * price - amountDiscount;
 
-alert('Цена покупки составит ' + TotalPaymentAmount +' Q');
+alert(`Цена покупки составит ${totalPaymentAmount} Q`);
 
 //Задача № 4
 let balance = 52334224;
-let CostOfGoods1 = 6500;
-let NumberOfGoodsPurchased = balance / CostOfGoods1;
-let RemainingMoney = balance % CostOfGoods1;
+let costGoods1 = 6500;
+let goodsPurchased = balance / costGoods1;
+let remainingMoney = balance % costGoods1;
 
-alert('Мы можем закупить ' + Math.trunc(NumberOfGoodsPurchased) +' единиц товара, после закупки на счету останется '+ RemainingMoney + ' Q');
+alert(`Мы можем закупить ${Math.trunc(goodsPurchased)} единиц товара, после закупки на счету останется ${remainingMoney} Q`);
