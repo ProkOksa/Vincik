43768b25a040e6a6819caa0ec2b9cfcd9369d81c
Oksana <prokoksa@gmail.com>
2022-10-21 23:35:28 +0300

    changes in tasks 1 and 2, completion of task 3

--- JavaScript/Дз4_function/JS_services.js
+++ JavaScript/Дз4_function/JS_services.js
@@ -1,32 +1,73 @@
 //Задача 1
-let warrantyValue = calcWarrantyCost(1);
-
-function calcWarrantyCost(years) {
-  switch (years) {
-    case 1:
-      return 1250;
-    case 2:
-      return 2300;
-    default:
+let years;
+let needForGuarantee = confirm('Нужна ли вам дополнительная гаранитя');
+if (needForGuarantee == true) {
+  function calcWarrantyCost(years = parseFloat(prompt('Укажите скрок дополнительной гарантии', '1 или 2'))) {
+    if (years !== 1 || years !== 2) {
+      alert('Вы указали что вам не нужна дополнительная гарантия')
       return 0;
+    } else {
+      alert('Спасибо!')
+      return years === 1 ? 1250 : 2300;
+    }
   }
+} else {
+  alert('Хорошего дня!')
 }
+
+let warrantyValue = calcWarrantyCost();
 
 console.log(`Дополнительное гарантийное обслуживание: ${warrantyValue} Q`);
 
 //Задача 2
-const string = 'Это пробный текст грайвировки';
-const words = string.split(' ');
+let qwe = confirm('Я хочу грайвировку!')
 
-let numberWordsEngraving = words.length;
-let priceOf1Word = 11;
+if (qwe == true) {
+  const string = prompt('Укажите текст грайвировки:', '');
+  const words = string.split(' ');
 
+  let numberWordsEngraving = !!string ? words.length : 0;
+  let priceOf1Word = 11;
 
-function calcEngravingCost(numberWordsEngraving) {
-  if (numberWordsEngraving === undefined) {
-    console.log(`Подарочная упаковка и гравировка: 0 Q`);
-  } else if (numberWordsEngraving > 0) {
-    console.log(`Подарочная упаковка и гравировка: ${numberWordsEngraving * priceOf1Word} Q`);
+  function calcEngravingCost(numberWordsEngraving) {
+    if (!numberWordsEngraving || string === 'undefined') {
+      console.log(`Подарочная упаковка и гравировка: 0 Q`);
+    } else if (numberWordsEngraving > 0) {
+      console.log(`Подарочная упаковка и гравировка: ${numberWordsEngraving * priceOf1Word} Q`);
+    }
+  }
+  calcEngravingCost(numberWordsEngraving);
+} else {
+  alert('Вам не нужна грайвировка. Хорошего дня. Заходите к нам еще.')
+  console.log(`Подарочная упаковка и гравировка: 0 Q`)
+}
+
+// Задача 3
+let deliv = delivery();
+
+function delivery(needForDelivery = prompt('Нужна ли вам доставка?', '')) {
+  if (needForDelivery === 'да') {
+    let deliveryRegion = prompt('Укажите куда доставить товар', '');
+    if (deliveryRegion == 'Луна') {
+      alert('Вы указади облость доставки Луна');
+      console.log('Стоимость доставки: 150 Q');
+    } else if (deliveryRegion == 'Крабовидная туманность') {
+      alert('Вы указади облость доставки Крабовидная туманность');
+      console.log('Стоимость доставки: 250 Q');
+    } else if (deliveryRegion == 'Галактика Туманность Андромеды') {
+      alert('Вы указади облость доставки Галактика Туманность Андромеды');
+      console.log('Стоимость доставки: 550 Q');
+    } else if (deliveryRegion == 'Туманность Ориона') {
+      alert('Вы указади облость доставки Туманность Ориона');
+      console.log('Стоимость доставки: 600 Q');
+    } else {
+      alert('В ваш регион доставка не осуществляется');
+      console.log('Доставка не требуется');
+    }
+  } else if (needForDelivery == 'нет') {
+    alert('Вам не ребуется доставка');
+    console.log('Доставка не требуется');
+  } else {
+    console.log('Ошибка при расчете стоимости доставки');
   }
 }
-calcEngravingCost(numberWordsEngraving);
