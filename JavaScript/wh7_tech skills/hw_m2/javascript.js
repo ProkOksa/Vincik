0e576bf57069498a0497f2f7cff714e4f641664d
Oksana <prokoksa@gmail.com>
2022-12-01 23:57:36 +0300

    task completed for modules 1-4

---
+++ JavaScript/wh7_tech skills/hw_m2/javascript.js
@@ -0,0 +1,22 @@
+// 1
+let num = prompt('Укажите число', '');
+let exp = prompt('Укажите степень', '');
+
+console.log(`${num} в степени ${exp} = ${Math.pow(num, exp)}`);
+
+//2
+let arr = [];
+
+for (let i = 0; i < 5; i++) {
+  arr[i] = prompt('Укажите имя', '');
+}
+
+alert('Имена внесены успешно');
+
+let user = prompt('Введите имя пользователя', '');
+
+if (arr.includes(user) === true) {
+  alert(`${user}, вы успешно вошли`);
+} else {
+  alert('Ошибка!');
+}
